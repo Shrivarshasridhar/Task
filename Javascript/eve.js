91f5e8c4431ab04017a55b173606a28d5b807f93
Shrivarsha <shrivarshak@gmail.com>
2025-01-11 10:03:01 +0530

    first change

---
+++ Javascript/eve.js
@@ -0,0 +1,38 @@
+/*let i;
+for(i=5;i<=50;i++){
+    if(i%5==0){
+    console.log(i);
+    }
+}*/
+/*let s=0;
+let i;
+for( i=0;i<=10;i++){
+  console.log(s+=i);
+}*/
+/*let i;
+for(i=50;i>=0;i--){
+  console.log(i);
+}*/
+
+/*for( i=0;i<=5;i++){
+  let str= "*";
+ console.log(str.repeat(i));
+}*/
+/*function a(){
+  console.log("abc");
+}
+a();*/
+
+/*function a(x,y){
+  console.log(x+y);
+}
+a(12,45);*/
+
+/*let abc=(x,y)=>console.log(x+y);
+abc(10,20);*/
+
+/*function n(x,y){
+  console.log(x , y +
+ "  Excited to collaborate and bring innovative ideas to fruition.Grateful for this opportunity to grow and make an impact.Looking forward to a successful journey together!");
+}
+n("varsha","tcs");*/
